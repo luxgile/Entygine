47756eb03aca16c0126aef191a2c1f6da0016d71
Luxgile <bisslit.12.2@gmail.com>
2020-07-30 21:57:49 +0200

    Implemented StructArray

---
+++ Entygine/Scripts/StructArray.cs
@@ -0,0 +1,84 @@
+﻿using Entygine.Mathematics;
+using OpenToolkit.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entygine
+{
+    public class StructArray<T0> where T0 : struct
+    {
+        private int size;
+        private int capacity;
+        private T0[] array;
+
+        private const int MIN_CAPACITY = 4;
+
+        public StructArray()
+        {
+            size = 0;
+            array = new T0[0];
+        }
+        public StructArray(int capacity, bool fill)
+        {
+            size = fill ? capacity : 0;
+            this.capacity = capacity;
+            array = new T0[capacity];
+        }
+
+        public ref T0 this[int index]
+        {
+            get
+            {
+                if (index >= size)
+                    throw new IndexOutOfRangeException();
+
+                return ref array[index];
+            }
+        }
+
+        public void Add(T0 value)
+        {
+            if (size >= capacity)
+            {
+                int newCapacity = capacity * 2;
+                if (newCapacity < MIN_CAPACITY)
+                    newCapacity = MIN_CAPACITY;
+
+                ResizeCapacity(newCapacity);
+            }
+
+            array[size] = value;
+            size++;
+        }
+
+        public void SwapForLast(T0 value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(value))
+                {
+                    array[i] = array[size - 1];
+                    size--;
+                    return;
+                }
+            }
+        }
+        public void SwapForLast(int index)
+        {
+            if (index >= size)
+                throw new IndexOutOfRangeException();
+
+            array[index] = array[size - 1];
+            size--;
+        }
+
+        private void ResizeCapacity(int capacity)
+        {
+            Array.Resize(ref array, capacity);
+            this.capacity = capacity;
+        }
+
+        public int Count => size;
+    }
+}
---
+++ Entygine_Tests/StructArray_Tests.cs
@@ -0,0 +1,78 @@
+﻿using Entygine.Ecs;
+using NUnit.Framework;
+
+namespace Entygine.Tests
+{
+    public class StructArray_Tests
+    {
+        [Test]
+        public void Adding_1()
+        {
+            StructArray<Entity> entities = new StructArray<Entity>();
+            Entity entity = new Entity() { id = 2, version = 1 };
+            entities.Add(entity);
+
+            Assert.AreEqual(entities.Count, 1);
+        }
+
+        [Test]
+        public void Adding_2()
+        {
+            StructArray<Entity> entities = new StructArray<Entity>();
+            Entity entity = new Entity() { id = 2, version = 1 };
+            entities.Add(entity);
+
+            entity.id = 3;
+            entities.Add(entity);
+
+            Assert.AreEqual(entities.Count, 2);
+        }
+
+        [Test]
+        public void Removing_1()
+        {
+            StructArray<Entity> entities = new StructArray<Entity>();
+            Entity entity = new Entity() { id = 2, version = 1 };
+            entities.Add(entity);
+
+            entity.id = 3;
+            entities.Add(entity);
+
+            entity.id = 2;
+            entities.SwapForLast(entity);
+
+            Assert.AreEqual(entities.Count, 1);
+        }
+
+        [Test]
+        public void Removing_2()
+        {
+            StructArray<Entity> entities = new StructArray<Entity>();
+            Entity entity = new Entity();
+            entities.Add(entity);
+
+            entity.id = 1;
+            entities.Add(entity);
+
+            entity.id = 2;
+            entities.Add(entity);
+
+            entity.id = 3;
+            entities.Add(entity);
+
+            entities.SwapForLast(0);
+
+            Assert.AreEqual(entities[0].id, 3);
+        }
+
+        [Test]
+        public void Referencing()
+        {
+            StructArray<Entity> entities = new StructArray<Entity>(8, true);
+            ref Entity entity = ref entities[0];
+            entity.id = 1;
+
+            Assert.AreEqual(entities[0].id, 1);
+        }
+    }
+}
