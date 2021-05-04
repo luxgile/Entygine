48f47157d82d97070f58cf1d6ca20594a84dd1b3
Luxgile <bisslit.12.2@gmail.com>
2021-05-05 01:03:56 +0200

    ECS iteration prettier

--- Entygine/Scripts/Common/Ecs/Systems/S_BoxHover.cs
+++ Entygine/Scripts/Common/Ecs/Systems/S_BoxHover.cs
@@ -6,33 +6,31 @@ namespace Entygine.Ecs.Systems
 
     public class S_BoxHover : BaseSystem
     {
-        private EntityQuerySettings query = new EntityQuerySettings().With(TypeCache.ReadType<C_BoxTag>(), TypeCache.WriteType<C_Position>());
+        private readonly EntityQuerySettings query = new EntityQuerySettings().With(TypeCache.ReadType<C_BoxTag>(), TypeCache.WriteType<C_Position>());
+        private EntityQueryScope scope;
+
+        protected override void OnSystemCreated()
+        {
+            scope = (EntityQueryScope)new EntityQueryScope().Setup(query).Iteration((context) =>
+            {
+                context.Read(out C_Position position);
+                position.value.y += (MathUtils.Sin((float)EntygineApp.EngineTime)) * 0.01f;
+                context.Write(position);
+            });
+        }
 
         protected override void OnPerformFrame(float dt)
         {
-            base.OnPerformFrame(dt);
-
-            IterateQuery(new BoxIterator(), query);
-
+            //TODO: Make new System Iterator where most of this is handled in the back
+            scope.OnlyChanged(LastVersionWorked).Perform();
+            asdasdasd
             //TODO: This is what iterations should look like, to avoid complicated stuff. Have in mind it should allow chunk iteration and entity iteration
-            SetupQuery(query).Iteration((context) =>
-            {
-                context.Read(out C_Position position);
-                position.value += (MathUtils.Sin((float)EntygineApp.EngineTime)) * 0.01f;
-                context.Write(position);
-            }).Perform();
-        }
-
-        private struct BoxIterator : IQueryEntityIterator
-        {
-            public void Iteration(ref EntityChunk chunk, int index)
-            {
-                if (chunk.TryGetComponent(index, out C_Position position))
-                {
-                    position.value.y += (MathUtils.Sin((float)EntygineApp.EngineTime)) * 0.01f;
-                    chunk.SetComponent(index, position);
-                }
-            }
+            //SetupQuery(query).Iteration((context) =>
+            //{
+            //    context.Read(out C_Position position);
+            //    position.value += (MathUtils.Sin((float)EntygineApp.EngineTime)) * 0.01f;
+            //    context.Write(position);
+            //}).Perform();
         }
     }
 }
---
+++ Entygine/Scripts/ECS Architecture/Queries/IQueryScope.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Entygine.Ecs
+{
+    public delegate void QueryDelegate<T0>(T0 context);
+    public interface IQueryContext { }
+    public abstract class QueryScope<T0> where T0 : struct, IQueryContext
+    {
+        protected QueryDelegate<T0> Iterator { get; private set; }
+        protected EntityQuerySettings Settings { get; private set; }
+        protected uint ChangeVersion { get; private set; } = 0;
+
+        public QueryScope<T0> Setup(EntityQuerySettings settings)
+        {
+            Settings = settings;
+            return this;
+        }
+        public QueryScope<T0> Iteration(QueryDelegate<T0> iterator)
+        {
+            Iterator = iterator;
+            return this;
+        }
+        public QueryScope<T0> OnlyChanged(uint version)
+        {
+            ChangeVersion = version;
+            return this;
+        }
+        public abstract void Perform();
+    }
+
+    public struct EntityQueryContext : IQueryContext
+    {
+        public EntityWorld World { get; init; }
+        public int Chunk { get; init; }
+        public int Index { get; init; }
+
+        public EntityQueryContext(EntityWorld world, int chunk, int index)
+        {
+            this.World = world;
+            this.Chunk = chunk;
+            this.Index = index;
+        }
+
+        public void Read<T0>(out T0 comp) where T0 : IComponent
+        {
+            ref EntityChunk ec = ref World.EntityManager.GetChunk(Chunk);
+            ec.TryGetComponent(Index, out comp);
+        }
+
+        public void Write<T0>(T0 comp) where T0 : IComponent
+        {
+            ref EntityChunk ec = ref World.EntityManager.GetChunk(Chunk);
+            ec.SetComponent(Index, comp);
+        }
+    }
+
+    public class EntityQueryScope : QueryScope<EntityQueryContext>
+    {
+        public override void Perform()
+        {
+            EntityWorld world = EntityWorld.Active;
+            bool generalWrite = Settings.IsGeneralWrite();
+            StructArray<EntityChunk> chunks = world.EntityManager.GetChunks();
+            for (int i = 0; i < chunks.Count; i++)
+            {
+                ref EntityChunk chunk = ref chunks[i];
+                if (!Settings.Matches(chunk.Archetype))
+                    continue;
+
+                if (!chunk.HasChanged(ChangeVersion))
+                    continue;
+
+                for (int e = 0; e < chunk.Count; e++)
+                {
+                    EntityQueryContext context = new(world, i, e);
+                    Iterator(context);
+                }
+
+                if (generalWrite && Settings.NeedsUpdate(ref chunk))
+                    chunk.UpdateVersion(world.EntityManager.Version);
+            }
+        }
+    }
+}
